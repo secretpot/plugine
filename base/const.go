df7a5118d71630c41be4f9aea3b80bab326b4ba2
Bee <benicebee@189.cn>
2024-12-21 01:33:29 +0800

    6th commit

--- base/const.go
+++ base/const.go
@@ -1,13 +1,10 @@
 package base
 
-type Const struct {
+type Plugin struct {
 	Name string
 	Settings interface{}
-}
 
-type Plugin struct {
 	Before func() error
 	After func() error
 	Callback func() error
-	Const
 }
