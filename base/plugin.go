f4fdde5107900fadfb3f29b750edd1aa50d35777
Bee <benicebee@189.cn>
2024-12-21 02:28:30 +0800

    11th commit

--- base/plugin.go
+++ base/plugin.go
@@ -4,6 +4,7 @@ package base
 type BasePlugin struct {
 	name     string
 	settings interface{}
+	functions map[string]interface{}
 }
 
 // Plugin 定义了插件必须实现的接口
@@ -12,10 +13,17 @@ type Plugin interface {
 	GetName() string
 	// 获取插件设置
 	GetSettings() interface{}
+	// 获取插件函数
+	GetFunction(name string) interface{}
+	GetAllFunctions() map[string]interface{}
 	// 设置插件名称
 	SetName(name string)
 	// 设置插件配置
 	SetSettings(settings interface{})
+	// 设置插件函数
+	SetFunction(name string, function interface{}) map[string]interface{}
+	// 打印
+	String() string
 
 	// 插件生命周期钩子
 	Before() error
@@ -44,6 +52,26 @@ func (b *BasePlugin) SetSettings(settings interface{}) {
 func (b *BasePlugin) Before() error   { return nil }
 func (b *BasePlugin) After() error    { return nil }
 func (b *BasePlugin) Callback() error { return nil }
+
+func (b *BasePlugin) String() string {
+	return b.name
+}
+
+func (b *BasePlugin) GetFunction(name string) interface{} {
+	return b.functions[name]
+}
+
+func (b *BasePlugin) GetAllFunctions() map[string]interface{} {
+	return b.functions
+}
+
+func (b *BasePlugin) SetFunction(name string, function interface{}) map[string]interface{} {
+	if b.functions == nil {
+		b.functions = make(map[string]interface{})
+	}
+	b.functions[name] = function
+	return b.functions
+}
 
 func NewBasePlugin(name string, settings interface{}) *BasePlugin {
 	return &BasePlugin{
